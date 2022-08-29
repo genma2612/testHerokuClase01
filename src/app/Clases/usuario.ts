21350a6314be32001f42b6147da1f6d8ec279f5d
genma2612 <manuelagustingonzalez@gmail.com>
2022-08-29 20:22:50 -0300

    Subir a Heroku again

--- src/app/Clases/usuario.ts
+++ src/app/Clases/usuario.ts
@@ -1,4 +1,9 @@
 export class Usuario {
     email:string;
     password:string;
+
+    public constructor(usuario:string, contraseña:string){
+        this.email = usuario;
+        this.password = contraseña;
+    }
 }
