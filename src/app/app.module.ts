7971cda497490ce53f0077303208eba02e8a47f0
genma2612 <manuelagustingonzalez@gmail.com>
2022-08-29 19:56:07 -0300

    login bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Usuario } from './Clases/usuario';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BienvenidoComponent } from './Componentes/bienvenido/bienvenido.component';
+import { ErrorComponent } from './Componentes/error/error.component';
+import { LoginComponent } from './Componentes/login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidoComponent,
+    ErrorComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
